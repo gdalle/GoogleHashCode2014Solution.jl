04e6e18f71f7f1e08dc6f76fac9203b2070d6f2b
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-09-10 15:59:03 +0200

    Notebook export

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,15 +8,12 @@ using Test
 Random.seed!(63)
 
 @testset verbose = true "HashCode2014Solution.jl" begin
-    @testset verbose = true "Code quality (Aqua.jl)" begin
+    @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(
             GoogleHashCode2014Solution;
             ambiguities=false,
             deps_compat=(; check_extras=false),
         )
-    end
-    @testset verbose = true "Code formatting (JuliaFormatter.jl)" begin
-        @test format(GoogleHashCode2014Solution; verbose=false, overwrite=false)
     end
     @testset "Greedy algorithm" begin
         include("greedy.jl")
